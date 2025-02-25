6ef5acab23766b639c51ecb14005f6e4cdc07f4f
Rafael Laidlaw <rafalaidlaw@gmail.com>
2025-02-25 15:45:00 -0500

    Tested scale and placement of hp bar. To do: Update original assets.

--- src/scenes/battle-scene.js
+++ src/scenes/battle-scene.js
@@ -31,51 +31,55 @@ export class BattleScene extends Phaser.Scene {
       color: '#7E3D3F',
       fontSize: '32px',
     });
-    this.add.container(556, 318, [
-      this.add
-        .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
-        .setOrigin(0),
-      playerMonsterName,
-      this.#createHealth(34, 34),
-      this.add.text(playerMonsterName.width + 35, 23, 'L5', {
-        color: '#ED474B',
-        fontSize: '28px',
-      }),
-      this.add.text(30, 55, 'HP', {
-        color: '#FF6505',
-        fontSize: '24px',
-        fontStyle: 'italic',
-      }),
-      this.add
-        .text(443, 80, '25/25', {
-          color: '#7E3D3F',
-          fontSize: '16px',
-        })
-        .setOrigin(1, 0),
-    ]);
+    this.add
+      .container(130, 110, [
+        this.add
+          .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
+          .setOrigin(0),
+        playerMonsterName,
+        this.#createHealth(34, 34),
+        this.add.text(playerMonsterName.width + 35, 23, 'L5', {
+          color: '#ED474B',
+          fontSize: '28px',
+        }),
+        this.add.text(30, 55, 'HP', {
+          color: '#FF6505',
+          fontSize: '24px',
+          fontStyle: 'italic',
+        }),
+        this.add
+          .text(443, 80, '25/25', {
+            color: '#7E3D3F',
+            fontSize: '16px',
+          })
+          .setOrigin(1, 0),
+      ])
+      .setScale(0.3, 0.3);
 
     // render out the enemy health bar
     const enemyMonsterName = this.add.text(30, 20, MONSTER_ASSET_KEYS.ENEMY, {
       color: '#7E3D3F',
       fontSize: '32px',
     });
-    this.add.container(0, 0, [
-      this.add
-        .image(15, 10, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
-        .setOrigin(0)
-        .setScale(0.35, 0.4),
-      enemyMonsterName,
-      this.#createHealth(34, 34),
-      this.add.text(enemyMonsterName.width + 35, 23, 'L5', {
-        color: '#ED474B',
-        fontSize: '28px',
-      }),
-      this.add.text(30, 55, 'HP', {
-        color: '#FF6505',
-        fontSize: '24px',
-        fontStyle: 'italic',
-      }),
-    ]);
+    this.add
+      .container(30, 13, [
+        this.add
+          .image(0, 0, BATTLE_ASSET_KEYS.HEALTH_BAR_BACKGROUND)
+          .setOrigin(0)
+          .setScale(1, 0.8),
+        enemyMonsterName,
+        this.#createHealth(34, 34),
+        this.add.text(enemyMonsterName.width + 35, 23, 'L5', {
+          color: '#ED474B',
+          fontSize: '28px',
+        }),
+        this.add.text(30, 55, 'HP', {
+          color: '#FF6505',
+          fontSize: '24px',
+          fontStyle: 'italic',
+        }),
+      ])
+      .setScale(0.3, 0.3);
   }
 
   #createHealth(x, y) {
